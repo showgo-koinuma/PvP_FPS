1acd4186e39444df9360dd076465ad8652236595
showgo-koinuma <85728773+showgo-koinuma@users.noreply.github.com>
2023-11-20 23:21:00 +0900

    [add] Lobby Scene, ButtonでのUIの遷移システム

---
+++ Assets/Game/Scripts/UI/CustomButton.cs
@@ -0,0 +1,39 @@
+using UnityEngine.EventSystems;
+using UnityEngine;
+using DG.Tweening;
+using System;
+using UnityEngine.Events;
+using Unity.VisualScripting;
+
+/// <summary>画像をボタンにする</summary>
+public class CustomButton : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler,
+    IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField, Tooltip("ボタンの実行内容")] public UnityEvent _buttonAction;
+    CanvasGroup _canvasGroup;
+
+    private void Awake() => _canvasGroup = GetComponent<CanvasGroup>();
+    
+    // タップ クリックしたときの処理を実行
+    public void OnPointerClick(PointerEventData eventData) 
+    {
+        _buttonAction?.Invoke(); // Actionが設定されてないときはDebugを出したい
+    }
+    // カーソルが重なる
+    public void OnPointerEnter(PointerEventData eventData) { }
+    // カーソルが離れる
+    public void OnPointerExit(PointerEventData eventData) { }
+
+    // クリックDown
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.DOScale(0.95f, 0.24f).SetEase(Ease.OutCubic);
+        _canvasGroup.DOFade(0.8f, 0.24f).SetEase(Ease.OutCubic);
+    }
+    // クリックUp
+    public void OnPointerUp(PointerEventData eventData) 
+    {
+        transform.DOScale(1f, 0.24f).SetEase(Ease.OutCubic);
+        _canvasGroup.DOFade(1f, 0.24f).SetEase(Ease.OutCubic);
+    }
+}
