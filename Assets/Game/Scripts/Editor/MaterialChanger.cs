09262116a5dd51ac92e43fd5145ee53fbc977649
showgo-koinuma <van71413989i@gmail.com>
2024-01-29 00:29:42 +0900

    [update] map material change

--- Assets/Game/Scripts/Editor/MaterialChanger.cs
+++ Assets/Game/Scripts/Editor/MaterialChanger.cs
@@ -1,12 +1,14 @@
 #if UNITY_EDITOR
 
 using UnityEditor;
-using UnityEditor.TerrainTools;
 using UnityEngine;
 
 public class MaterialChanger : EditorWindow
 {
-    static Material _material;
+    static Material _materialQ;
+    static Material _materialE;
+    static Material _materialR;
+    static Material _materialF;
     static Vector2 _mousePos;
 
 
@@ -18,7 +20,10 @@ public class MaterialChanger : EditorWindow
 
     private void OnGUI()
     {
-        _material = EditorGUILayout.ObjectField("material", _material, typeof(Material), false) as Material;
+        _materialQ = EditorGUILayout.ObjectField("Material Q", _materialQ, typeof(Material), false) as Material;
+        _materialE = EditorGUILayout.ObjectField("Material E", _materialE, typeof(Material), false) as Material;
+        _materialR = EditorGUILayout.ObjectField("Material R", _materialR, typeof(Material), false) as Material;
+        _materialF = EditorGUILayout.ObjectField("Material F", _materialF, typeof(Material), false) as Material;
         wantsMouseMove = true;
     }
 
@@ -34,8 +39,8 @@ public class MaterialChanger : EditorWindow
         _mousePos = Event.current.mousePosition;
     }
 
-    [MenuItem ("Tools/MaterialChanger/Change the Material of the Selected Object #g")]
-    static void ChangingTheSelectedMaterial()
+    [MenuItem ("Tools/MaterialChanger/Change Material on Q #q")]
+    static void ChangingMaterialQ()
     {
         var picked = HandleUtility.PickGameObject(_mousePos, false);
 
@@ -45,10 +50,54 @@ public class MaterialChanger : EditorWindow
 
             if (picked.TryGetComponent(out MeshRenderer meshR))
             {
-                meshR.sharedMaterial = _material;
+                meshR.sharedMaterial = _materialQ;
+            }
+        }
+    }
+    [MenuItem("Tools/MaterialChanger/Change Material on E #e")]
+    static void ChangingMaterialE()
+    {
+        var picked = HandleUtility.PickGameObject(_mousePos, false);
+
+        if (picked != null)
+        {
+            Debug.Log(picked.name);
+
+            if (picked.TryGetComponent(out MeshRenderer meshR))
+            {
+                meshR.sharedMaterial = _materialE;
+            }
+        }
+    }
+    [MenuItem("Tools/MaterialChanger/Change Material on R #r")]
+    static void ChangingMaterialR()
+    {
+        var picked = HandleUtility.PickGameObject(_mousePos, false);
+
+        if (picked != null)
+        {
+            Debug.Log(picked.name);
+
+            if (picked.TryGetComponent(out MeshRenderer meshR))
+            {
+                meshR.sharedMaterial = _materialR;
+            }
+        }
+    }
+    [MenuItem("Tools/MaterialChanger/Change Material on F #c")]
+    static void ChangingMaterialF()
+    {
+        var picked = HandleUtility.PickGameObject(_mousePos, false);
+
+        if (picked != null)
+        {
+            Debug.Log(picked.name);
+
+            if (picked.TryGetComponent(out MeshRenderer meshR))
+            {
+                meshR.sharedMaterial = _materialF;
             }
         }
     }
 }
-
 #endif
